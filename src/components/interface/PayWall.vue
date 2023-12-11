41e24dfdb8108e471687d4e58fd678b3362dc147
vswee <github@sinaswee.com>
2023-12-11 23:56:29 +0000

    improve wallet-connect

---
+++ src/components/interface/PayWall.vue
@@ -0,0 +1,51 @@
+<template lang="">
+  <div class="modal-stationary">
+    <h1><i class="i-alert-circle"></i> Message</h1>
+    <template v-if="message==='needZH'">
+    <p>Only Holders of the ZH token may participate on this platform.<br>
+       Check back soon for details on acquiring ZH, or email <a href="mailto:zetta@zettahash.org" class="link">zetta@zettahash.org</a> for more information.</p>
+    </template>
+
+    <template v-if="message==='needAcc'">
+    <p>Please connect your wallet to get started.</p>
+    </template>
+  </div>
+</template>
+<script>
+import { mapGetters } from 'vuex';
+// eslint-disable-next-line no-unused-vars
+export default {
+  name: 'PayWall',
+  data() {
+    return {
+      // message: '',
+    }
+  },
+  computed: {
+    ...mapGetters({
+      application: 'application',
+    }),
+    message() {
+
+      if(!this.application.walletConnected){return 'needAcc'}
+      if (!this.application.zhHolderBool) {return 'needZH'}
+      return '?'
+    },
+  },
+}
+</script>
+<style lang="scss" scoped>
+  .modal-stationary{
+    margin:50px auto auto auto;
+    height: auto;
+    width:500px;
+    max-width:80vw;
+    padding:30px;
+    border-radius: 20px;
+    background: var(--neutral-9);
+    h1{
+      margin-top:0;
+    }
+    p{margin-bottom:0;}
+  }
+</style>
--- src/components/interface/WalletConnector.vue
+++ src/components/interface/WalletConnector.vue
@@ -1,26 +1,26 @@
 <template lang="">
   <div>
-  <div class="w3m-core-button"></div>
-    <!-- <w3m-core-button></w3m-core-button> -->
+  <div class="w3m-button" balance="show"></div>
+  <!-- <w3m-network-button disabled/> -->
+    <!-- <w3m-button></w3m-button> -->
   </div>
 </template> 
 <script>
 import { mapGetters } from 'vuex';
-import { configureChains, createClient } from "@wagmi/core";
-import { mainnet, goerli } from "@wagmi/core/chains";
- 
-import {
-  EthereumClient,
-  w3mConnectors,
-  w3mProvider,
-} from "@web3modal/ethereum";
-import { Web3Modal } from "@web3modal/html";
+
+// eslint-disable-next-line no-unused-vars
+import { readContract, getAccount, fetchBalance, watchAccount } from '@wagmi/core'
+// eslint-disable-next-line no-unused-vars
+import { createWeb3Modal, defaultWagmiConfig, useWeb3ModalEvents } from '@web3modal/wagmi/vue'
+import { mainnet } from 'viem/chains'
+// import { genericABI } from '../data/genericABI'
 
 export default {
   name: "WalletConnector",
-  // components:['w3m-core-button'],
+  // components:['w3m-button'],
   data() {
     return {
+      zettahash: '0x2C0e15190aCB858Bf74447928Cbd8Fb9709dCB19',
       checkConnectionInterval: false,
       network: false,
       buttonConnectUI: false,
@@ -30,132 +30,98 @@ export default {
     ...mapGetters({
       application: 'application',
     }),
+    wagmiConnected(){return this.application['wagmi.connected']},
   },
   mounted() {
-    const html = document.createElement('w3m-core-button')
-    document.querySelector(".w3m-core-button").replaceWith(html)
+    const html = document.createElement('w3m-button')
+    document.querySelector(".w3m-button").replaceWith(html)
     this.init()
+  },
+  watch: {
+    wagmiConnected(value) {
+      console.log("wagmiConnected:", value)
+    },
   },
   methods: {
     async init() {
-      // 1. Define constants
+      // 1. Get projectId at https://cloud.walletconnect.com
       const projectId = process.env.VUE_APP_WALLET_CONNECT_PROJECT_ID;
-      const chains = [goerli, mainnet];
 
-      // 2. Configure wagmi client
-      const { provider } = configureChains(chains, [w3mProvider({ projectId })]);
-      const wagmiClient = createClient({
-        autoConnect: true,
-        connectors: w3mConnectors({ chains, version: 1, projectId }),
-        provider,
-      });
+      // 2. Create wagmiConfig
+      const metadata = {
+        name: 'Web3Modal',
+        description: 'Web3Modal',
+        url: 'https://web3modal.com',
+      }
 
-      // 3. Create ethereum and modal clients
-      const ethereumClient = new EthereumClient(wagmiClient, chains);
-      const web3modal = new Web3Modal(
-        {
-          projectId,
-          walletImages: {
-            safe: "https://pbs.twimg.com/profile_images/1566773491764023297/IvmCdGnM_400x400.jpg",
-          },
+      const chains = [mainnet]
+      const wagmiConfig = defaultWagmiConfig({ chains, projectId, metadata })
+
+      // 3. Create modal
+      const themeMode = 'dark'
+      const themeVariables = {
+        '--w3m-font-family': "'Roboto', Arial, Helvetica, sans-serif",
+        '--w3m-accent': '#0062ff',
+        '--w3m-color-mix': '#3783ff',
+      }
+      const tokens = {
+        1: {
+          address: this.zettahash,
         },
-        ethereumClient
-      );
-      web3modal.setDefaultChain(mainnet)
-
-      web3modal.setTheme({
-        themeMode: 'dark',
-        themeVariables: {
-          '--w3m-font-family': 'Roboto, Arial, Helvetica, sans-serif',
-          '--w3m-z-index': '89',
-          '--w3m-background-border-radius': '20px',
-          '--w3m-container-border-radius': '20px',
-          '--w3m-container-border-width': '20px',
-          '--w3m-wallet-icon-border-radius': '15px',
-          '--w3m-input-border-radius': '28px',
-          '--w3m-button-border-radius': '100px',
-          '--w3m-notification-border-radius': '36px',
-          '--w3m-secondary-button-border-radius': '28px',
-          '--w3m-icon-button-border-radius': '50%',
-          '--w3m-button-hover-highlight-border-radius': '10px',
-          '--w3m-text-big-bold-size': '20px',
-          '--w3m-text-big-bold-weight': '500',
-          '--w3m-text-big-bold-line-height': '24px',
-          '--w3m-text-big-bold-letter-spacing': '-0.03em',
-          '--w3m-text-big-bold-text-transform': 'none',
-          '--w3m-text-xsmall-bold-size': '10px',
-          '--w3m-text-xsmall-bold-weight': '500',
-          '--w3m-text-xsmall-bold-line-height': '12px',
-          '--w3m-text-xsmall-bold-letter-spacing': '0.02em',
-          '--w3m-text-xsmall-bold-text-transform': 'uppercase',
-          '--w3m-text-xsmall-regular-size': '12px',
-          '--w3m-text-xsmall-regular-weight': '500',
-          '--w3m-text-xsmall-regular-line-height': '14px',
-          '--w3m-text-xsmall-regular-letter-spacing': '-0.03em',
-          '--w3m-text-xsmall-regular-text-transform': 'none',
-          '--w3m-text-small-thin-size': '14px',
-          '--w3m-text-small-thin-weight': '500',
-          '--w3m-text-small-thin-line-height': '16px',
-          '--w3m-text-small-thin-letter-spacing': '-0.03em',
-          '--w3m-text-small-thin-text-transform': 'none',
-          '--w3m-text-small-regular-size': '14px',
-          '--w3m-text-small-regular-weight': '500',
-          '--w3m-text-small-regular-line-height': '16px',
-          '--w3m-text-small-regular-letter-spacing': '-0.03em',
-          '--w3m-text-small-regular-text-transform': 'none',
-          '--w3m-text-medium-regular-size': '16px',
-          '--w3m-text-medium-regular-weight': '500',
-          '--w3m-text-medium-regular-line-height': '-18px',
-          '--w3m-text-medium-regular-letter-spacing': '-0.03em',
-          '--w3m-text-medium-regular-text-transform': 'none',
-          '--w3m-success-color': 'rgb(38,181,98)',
-          '--w3m-error-color': 'rgb(242, 90, 103)',
-          '--w3m-background-image-url': 'none',
-        }
-      })
-
-      // await console.log(ethereumClient, web3modal)
-//       window.ethereum.on('accountsChanged', function (accounts) {
-//   // Time to reload your interface with accounts[0]!
-// })
-
-// window.ethereum.on('networkChanged', function (networkId) {
-//   // Time to reload your interface with the new networkId
-// })
-// const account = getAccount()
-
-//       this.$store.commit('setDynamic', {
-//         item: 'wallet',
-//         value: account
-//       })
+      }
+      createWeb3Modal({ wagmiConfig, projectId, chains, themeMode, themeVariables, tokens })
       this.ui()
       window.globalSelf = this
-      window.addEventListener("focus", ()=>{window.globalSelf.ui()})
-      window.addEventListener("mousemove", ()=>{window.globalSelf.ui()})
-      window.addEventListener("click", ()=>{window.globalSelf.ui()})
+      watchAccount((account) => window.globalSelf.updateAccount(account))
+
+      window.addEventListener("focus", () => { window.globalSelf.ui() })
+      window.addEventListener("mousemove", () => { window.globalSelf.ui() })
+      window.addEventListener("click", () => { window.globalSelf.ui() })
     },
     async ui() {
+    },
+    async updateAccount(account) {
+      const { isConnected } = getAccount()
+      if (account.isConnected) {
+        const balance = await fetchBalance({
+          address: account.address,
+          token: this.zettahash,
+        })
+        let holderBool = Number(balance.formatted) > 0 ? true : false
 
-      this.buttonConnectUI = setInterval(() => {
-        const core = document.querySelector('w3m-core-button')
-        if (core.shadowRoot && core.shadowRoot.querySelectorAll('w3m-account-button').length > 0) {
-          const btnBig = core.shadowRoot.querySelector('w3m-account-button').shadowRoot.querySelector('w3m-button-big')
-          btnBig.querySelector('w3m-avatar').style['margin-right'] = "5px"
-          btnBig.shadowRoot.querySelector('button').style.height = "40px"
-          btnBig.shadowRoot.querySelector('button').style.padding = "0px calc(1.5rem / 1.62)"
-          clearInterval(this.buttonConnectUI)
-          this.buttonConnectUI = false
-        }
-        if (core.shadowRoot && core.shadowRoot.querySelectorAll('w3m-connect-button').length > 0) {
-          const btnBig = core.shadowRoot.querySelector('w3m-connect-button').shadowRoot.querySelector('w3m-button-big')
-          btnBig.querySelector('w3m-text').style['margin-left'] = "5px"
-          btnBig.shadowRoot.querySelector('button').style.height = "40px"
-          btnBig.shadowRoot.querySelector('button').style.padding = "0px calc(1.5rem / 1.62)"
-          clearInterval(this.buttonConnectUI)
-          this.buttonConnectUI = false
-        }
-      }, 100)
-    }
+        this.$store.commit('setDynamic', {
+          item: 'zhHolderBool',
+          value: holderBool
+        })
+        this.$store.commit('setDynamic', {
+          item: 'zhBalance',
+          value: balance.formatted
+        })
+      } else {
+        this.$store.commit('setDynamic', {
+          item: 'zhHolderBool',
+          value: false
+        })
+        this.$store.commit('setDynamic', {
+          item: 'zhBalance',
+          value: '0.00'
+        })
+      }
+
+      if (isConnected) {
+
+        this.$store.commit('setDynamic', {
+          item: 'walletConnected',
+          value: true
+        })
+      } else {
+
+        this.$store.commit('setDynamic', {
+          item: 'walletConnected',
+          value: false
+        })
+      }
+    },
   },
 }
 </script>
